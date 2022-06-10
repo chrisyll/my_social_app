db7f28c259bdb88af88342afe1b1e253abb91bd0
Chris <chrisyll@csd.auth.gr>
2022-06-10 13:04:00 +0300

    update

---
+++ src/components/NavigationItems/NavigationItems.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MyAux from '../../hoc/MyAux/MyAux';
+
+
+function NavigationItems() {
+    return ( 
+        <MyAux >
+            <div>
+        
+            </div>
+        </MyAux>
+    );
+}
+
+export default NavigationItems;
---
+++ src/containers/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class MainPage extends Component {
+    state = {  } 
+    render() { 
+        return (
+            
+        );
+    }
+}
+ 
+export default MainPage;
